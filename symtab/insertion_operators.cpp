d2b483428ed957dddd8987f06a6f9eedb7058b43
Tim Haines <thaines.astro@gmail.com>
2021-06-28 22:09:26 -0500

    Add insertion operator test for symtab

---
+++ symtab/insertion_operators.cpp
@@ -0,0 +1,62 @@
+#include "Aggregate.h"
+#include "Function.h"
+#include "Module.h"
+#include "StringTable.h"
+#include "Symbol.h"
+#include "Symtab.h"
+#include "Variable.h"
+
+#include <iostream>
+
+namespace st = Dyninst::SymtabAPI;
+
+int main() {
+  {
+    st::ModRange x;
+    std::cout << x;
+  }
+  {
+    st::Module *x;
+    std::cout << *x;
+  }
+  {
+    st::Statement x;
+    std::cout << x;
+  }
+  {
+    st::StringTable x;
+    std::cout << x;
+  }
+  {
+    st::StringTableEntry *x;
+    std::cout << *x;
+  }
+  {
+    st::Symbol x;
+    std::cout << x;
+  }
+  {
+    st::Variable x;
+    std::cout << x;
+  }
+  {
+    st::relocationEntry x;
+    std::cout << x;
+  }
+  {
+    st::Aggregate *x;
+    std::cout << *x;
+  }
+  {
+    st::Function x;
+    std::cout << x;
+  }
+  {
+    st::Variable x;
+    std::cout << x;
+  }
+  {
+    st::ExceptionBlock x;
+    std::cout << x;
+  }
+}
