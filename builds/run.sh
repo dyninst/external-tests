e19ed46802f6915bdf0059d8d5f0f2da5181d644
Tim Haines <thaines.astro@gmail.com>
2022-09-07 10:28:24 -0500

    Simplify builds/run.sh

--- builds/run.sh
+++ builds/run.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
 
-# Create or update dyninst
 if test ! -e dyninst; then
   git clone --depth 1 --branch master https://github.com/dyninst/dyninst
-else
-  git -C dyninst pull --ff-only origin master
 fi
 
-# Create or update the test suite
 if test ! -e testsuite; then
   git clone --depth 1 --branch master https://github.com/dyninst/testsuite
-else
-  git -C testsuite pull --ff-only origin master
 fi
 
-# Create or update spack
-if test ! -e spack; then
-  git clone --depth 1 --branch develop https://github.com/spack/spack
-
-  # Use the E4S binary cache to speed up installs
-  spack/bin/spack mirror add E4S https://cache.e4s.io
-  spack/bin/spack buildcache keys --install --trust
-else
-  git -C spack pull --ff-only origin develop
-fi
-
-declare -a packages=(cmake tbb)
-declare -a commands=(setup.sh build.sh test.sh upload.sh cleanup.sh)
+declare -a packages=(tbb)
+declare -a commands=(build.sh test.sh upload.sh cleanup.sh)
 
 for p in "${packages[@]}"; do
   echo Processing package $p
   cd $p
+  bash setup.sh ./dyninst ./testsuite
   for c in "${commands[@]}"; do
     echo Executing $c
     bash $c
