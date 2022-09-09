800871ba37f1fb2986cfd3fe3599dca6995e42e0
Tim Haines <thaines.astro@gmail.com>
2022-09-09 14:10:09 -0500

    Create spack env in setup.sh

--- builds/tbb/setup.sh
+++ builds/tbb/setup.sh
@@ -12,3 +12,16 @@ git clone --depth 1 --branch develop https://github.com/spack/spack
 # Use the E4S binary cache to speed up installs
 spack/bin/spack mirror add E4S https://cache.e4s.io
 spack/bin/spack buildcache keys --install --trust
+
+# Install dependencies
+. spack/share/spack/setup-env.sh
+spack env create -d .
+spack config add 'concretizer:unify:true'
+spack env activate .
+spack external find --not-buildable gcc autoconf bzip2 git tar xz perl cmake m4 ncurses
+spack add cmake
+spack add perl
+spack add boost+atomic+chrono+date_time+filesystem+system+thread+timer+container+random+exception
+spack add elfutils
+spack add libiberty+pic
+spack install --reuse
