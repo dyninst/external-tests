b6921cd301c807db6f4f6b2018b7dc0f937c953e
Tim Haines <thaines.astro@gmail.com>
2022-09-09 14:30:41 -0500

    E4S cache is used by default

--- builds/tbb/setup.sh
+++ builds/tbb/setup.sh
@@ -12,10 +12,6 @@ ln -s $2 testsuite
 # Grab a fresh spack
 git clone --depth 1 --branch develop https://github.com/spack/spack
 
-# Use the E4S binary cache to speed up installs
-spack/bin/spack mirror add E4S https://cache.e4s.io
-spack/bin/spack buildcache keys --install --trust
-
 # Install dependencies
 . spack/share/spack/setup-env.sh
 spack env create -d .
