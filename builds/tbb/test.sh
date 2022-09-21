cae46e131035f49e3335562886096947c50a4f88
Tim Haines <thaines.astro@gmail.com>
2022-09-21 00:18:45 -0500

    Remove TBB_ROOT_DIR calculation
    It isn't needed because find_package will search <prefix>/cmake automatically

--- builds/tbb/test.sh
+++ builds/tbb/test.sh
@@ -14,11 +14,8 @@ while IFS= read -r package; do
     continue
   fi
 
-  # TBB was the last thing loaded by spack, so it's the first entry in CMAKE_PREFIX_PATH
-  tbb_loc=$(echo $CMAKE_PREFIX_PATH | awk '{split($0,x,":"); print x[1]}')
-
   rm -f build.log
-  perl testsuite/scripts/build/build.pl --njobs=2 --purge --no-run-tests --dyninst-cmake-args="-DTBB_ROOT_DIR=\"${tbb_loc}/cmake\""
+  perl testsuite/scripts/build/build.pl --njobs=2 --purge --no-run-tests
   if test $? != 0; then
     echo Failed to run tests with $package >&2
     spack unload $package
