9690617f6c3c8646fd15ee8e0c38fb3e7a6f0032
Tim Haines <thaines.astro@gmail.com>
2022-11-12 17:30:40 -0600

    Ready for production run

--- builds/tbb/run.sh
+++ builds/tbb/run.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-#ln -s /dyninst/src dyninst
-#ln -s /testsuite/src testsuite
-#ln -s /spack spack
+ln -s /dyninst/src dyninst
+ln -s /testsuite/src testsuite
+ln -s /spack spack
 
 base_dir=$PWD
 
-. spack/share/spack/setup-env.sh
+. ./spack/share/spack/setup-env.sh
 rm -f install.log test.log
 
 for s in $(perl parse_specs.pl); do
@@ -25,10 +25,8 @@ for s in $(perl parse_specs.pl); do
     continue
   fi
   
-  rm -rf dyninst-build-$s
-  mkdir dyninst-build-$s
-  cd dyninst-build-$s
-  cmake ../dyninst -DCMAKE_INSTALL_PREFIX=$PWD -DDYNINST_WARNINGS_AS_ERRORS >/dev/null 2>&1
+  rm -rf dyninst-build-$s; mkdir dyninst-build-$s; cd dyninst-build-$s
+  cmake ../dyninst -DCMAKE_INSTALL_PREFIX=$PWD -DDYNINST_WARNINGS_AS_ERRORS=ON >/dev/null 2>&1
   cmake --build . --parallel ${BUILD_TEST_NUM_JOBS} >/dev/null 2>&1
   cmake --install . >/dev/null 2>&1
   if test $? != 0; then
@@ -37,10 +35,7 @@ for s in $(perl parse_specs.pl); do
     continue
   fi
   
-  cd $base_dir
-  rm -rf testsuite-build-$s
-  mkdir testsuite-build-$s
-  cd testsuite-build-$s
+  cd $base_dir; rm -rf testsuite-build-$s; mkdir testsuite-build-$s; cd testsuite-build-$s
   cmake ../testsuite -DCMAKE_INSTALL_PREFIX=$PWD -DDyninst_DIR=$(realpath $PWD/../dyninst-build-$s/lib/cmake/Dyninst) >/dev/null 2>&1
   cmake --build . --parallel ${BUILD_TEST_NUM_JOBS} >/dev/null 2>&1
   cmake --install . >/dev/null 2>&1
