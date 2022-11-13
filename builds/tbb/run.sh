47e50aa3ee6f22684e2fc66657df0784a8448a41
Tim Haines <thaines.astro@gmail.com>
2022-11-13 00:23:51 -0600

    Clean up runner

--- builds/tbb/run.sh
+++ builds/tbb/run.sh
@@ -3,46 +3,48 @@
 . ./spack/share/spack/setup-env.sh
 rm -f install.log test.log
 
-for s in $(perl parse_specs.pl); do
-  cd $base_dir
-  echo -n "$s... "
-  spack install -j${BUILD_TEST_NUM_JOBS} $s >>install.log 2>&1
+for spec in $(perl parse_specs.pl); do
+  echo -n "${spec}... "
+  spack install -j${BUILD_TEST_NUM_JOBS} ${spec} >>install.log 2>&1
   if test $? != 0; then
     echo "Failed to install" >&2
     continue
   fi
-  
-  spack load $s
+
+  spack load ${spec}
   if test $? != 0; then
     echo "Failed to load module" >&2
-    spack unload $s
+    spack unload ${spec}
     continue
   fi
-  
-  rm -rf dyninst-build-$s; mkdir dyninst-build-$s; cd dyninst-build-$s
-  cmake ../dyninst -DCMAKE_INSTALL_PREFIX=$PWD -DDYNINST_WARNINGS_AS_ERRORS=ON >/dev/null 2>&1
+
+  cd /
+  rm -rf dyninst-build testsuite-build
+
+  mkdir dyninst-build
+  cd dyninst-build
+  cmake /dyninst/src -DCMAKE_INSTALL_PREFIX=$PWD -DDYNINST_WARNINGS_AS_ERRORS=ON >/dev/null 2>&1
   cmake --build . --parallel ${BUILD_TEST_NUM_JOBS} >/dev/null 2>&1
   cmake --install . >/dev/null 2>&1
   if test $? != 0; then
     echo "Failed" >&2
-    spack unload $s
+    spack unload ${spec}
     continue
   fi
-  
-  cd $base_dir; rm -rf testsuite-build-$s; mkdir testsuite-build-$s; cd testsuite-build-$s
-  cmake ../testsuite -DCMAKE_INSTALL_PREFIX=$PWD -DDyninst_DIR=$(realpath $PWD/../dyninst-build-$s/lib/cmake/Dyninst) >/dev/null 2>&1
+
+  mkdir testsuite-build
+  cd testsuite-build
+  cmake /testsuite/src -DCMAKE_INSTALL_PREFIX=$PWD -DDyninst_DIR=/dyninst-build/lib/cmake/Dyninst >/dev/null 2>&1
   cmake --build . --parallel ${BUILD_TEST_NUM_JOBS} >/dev/null 2>&1
   cmake --install . >/dev/null 2>&1
   if test $? != 0; then
     echo "Failed" >&2
-    spack unload $s
+    spack unload ${spec}
     continue
   fi
 
   # Don't care if these fail
-  cd $base_dir
-  rm -rf dyninst-build-$s testsuite-build-$s
-  spack unload $s
-  spack uninstall -y $s >/dev/null 2>&1
+  spack unload ${spec}
+  spack uninstall -y ${spec} >/dev/null 2>&1
   echo "OK"
 done
