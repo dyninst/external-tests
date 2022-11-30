bacfc4dcae4c04e07da768eb766b772c130f214c
Tim Haines <thaines.astro@gmail.com>
2022-11-29 21:08:23 -0600

    Add libexpat and libarchive for CMake builds

--- docker/ubuntu/Dockerfile.cmake
+++ docker/ubuntu/Dockerfile.cmake
@@ -12,7 +12,9 @@ RUN apt -qq install -y --no-install-recommends \
     git             \
     curl            \
     libncurses-dev  \
-    libssl-dev
+    libssl-dev		\
+    libexpat1-dev	\
+    libarchive-dev
     
 
 RUN git clone --depth 1 --branch master https://github.com/dyninst/testsuite testsuite/src
@@ -21,9 +23,11 @@ RUN pip3 install clingo && \
     git clone --depth 1 --branch develop https://github.com/spack/spack && \
     ln -s /spack/bin/spack /usr/bin/spack && \
     spack compiler find && \
-    spack external find --not-buildable gcc && \
+    spack external find --not-buildable gcc curl && \
     ssl_version=$(apt show libssl-dev | perl -ne 'print $1 if m/version\:\s*(\d+\.\d+\.\d+\w)/i') && \
     ncurses_version=$(apt show libncurses-dev | perl -ne 'print $1 if m/version\:\s*(\d+\.\d+)/i') && \
+    expat_version=$(apt show libexpat1-dev | perl -ne 'print $1 if m/version\:\s*(\d+\.\d+\.\d+)/i') && \
+    archive_version=$(apt show libarchive-dev | perl -ne 'print $1 if m/version\:\s*(\d+\.\d+\.\d+)/i') && \
 printf "\
   openssl:\n\
     externals:\n\
@@ -35,9 +39,19 @@ printf "\
     - spec: ncurses@${ncurses_version}\n\
       prefix: /usr\n\
     buildable: false\n\
+  expat:\n\
+    externals:\n\
+    - spec: expat@${expat_version}\n\
+      prefix: /usr\n\
+    buildable: false\n\
+  libarchive:\n\
+    externals:\n\
+    - spec: libarchive@${archive_version}\n\
+      prefix: /usr\n\
+    buildable: false\n\
 " >> ~/.spack/packages.yaml
 
-COPY builds/cmake/* /
+COPY builds/cmake/* builds/run.sh /
 
 # Remove system cmake so it doesn't interfere
 RUN apt -qq remove --purge -y cmake*
