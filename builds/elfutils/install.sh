39ba80c20ecba26f21dab1cef44fe459a42293f5
Tim Haines <thaines.astro@gmail.com>
2023-03-30 15:34:46 -0500

    elfutils: fetch versions from git repo

--- builds/elfutils/install.sh
+++ builds/elfutils/install.sh
@@ -5,8 +5,24 @@ if x"${build_jobs}" = "x"; then
   build_jobs=1
 fi
 
-for v in 0.186 0.187 0.188 0.189; do
-  if [ -d $v ]; then continue; fi
+# Fetch the versions
+git clone --depth=1 git://sourceware.org/git/elfutils.git
+cd elfutils
+git fetch --tags
+declare -a versions=$(git tag --list | grep "elfutils" | sed 's/elfutils-//' | sort -rV)
+
+# Get the minimum version from Dyninst
+cd /
+rm -f dependencies.versions
+wget --no-check-certificate https://raw.githubusercontent.com/dyninst/dyninst/cmake_modernization/docker/dependencies.versions
+min_version=$(grep elfutils dependencies.versions | awk '{split($0,a,":"); print a[2]}')
+
+# Clean up
+rm -rf /elfutils dependencies.versions
+
+for v in ${versions}; do
+  if [[ $v < $min_version ]]; then continue; fi
+  echo $v >>versions.txt
   wget --no-check-certificate https://sourceware.org/elfutils/ftp/${v}/elfutils-${v}.tar.bz2
   tar -xf elfutils-${v}.tar.bz2
   cd elfutils-${v}/
