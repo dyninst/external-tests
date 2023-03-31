1790268b48ec06a9893bc26dad78d25f9ca5de3b
Tim Haines <thaines.astro@gmail.com>
2023-03-31 15:10:37 -0500

    deps: finalize updates

--- builds/deps_from_latest_source/test.sh
+++ builds/deps_from_latest_source/test.sh
@@ -13,8 +13,8 @@ for d in elfutils boost tbb dyninst; do
 done
 
 cd /elfutils-src
-mkdir build; cd $_
-../configure --disable-libdebuginfod --disable-debuginfod --prefix=/elfutils
+autoreconf -i -f
+./configure --disable-libdebuginfod --disable-debuginfod --prefix=/elfutils --enable-maintainer-mode
 make install -j${build_jobs}
 
 cd /boost-src
@@ -22,12 +22,10 @@ cd /boost-src
 ./b2 --prefix=/boost --layout=versioned variant=release link=shared threading=multi runtime-link=shared install -j${build_jobs}
 
 cd /tbb-src
-mkdir build; cd $_
 cmake . -DCMAKE_INSTALL_PREFIX=/tbb -DTBB_TEST=OFF -DCMAKE_BUILD_TYPE=Release
 cmake --build . --parallel ${build_jobs}
 cmake --install .
 
 cd /dyninst-src
-mkdir build; cd $_
-cmake .. -DBoost_ROOT_DIR=/boost -DTBB_ROOT_DIR=/tbb -DElfUtils_ROOT_DIR=/elfutils -DDYNINST_WARNINGS_AS_ERRORS=ON
+cmake . -DBoost_ROOT_DIR=/boost -DTBB_ROOT_DIR=/tbb -DElfUtils_ROOT_DIR=/elfutils -DDYNINST_WARNINGS_AS_ERRORS=ON
 cmake --build . --parallel $build_jobs
