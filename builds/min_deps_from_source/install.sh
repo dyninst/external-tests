7479346611a08787ca9f5bdc8c48c07ddca7684e
Tim Haines <thaines.astro@gmail.com>
2023-03-31 13:06:13 -0500

    Update min_deps_from_source

--- builds/min_deps_from_source/install.sh
+++ builds/min_deps_from_source/install.sh
@@ -1,54 +1,6 @@
 #!/bin/bash
 
-build_jobs=$1
-if test x"${build_jobs}" = "x"; then
-  build_jobs=1
-fi
-
-version=$(grep elfutils dyninst/docker/dependencies.versions | awk '{split($0,a,":"); print a[2]}')
-wget --no-check-certificate https://sourceware.org/elfutils/ftp/${version}/elfutils-${version}.tar.bz2
-bunzip2 elfutils-${version}.tar.bz2
-tar -xf elfutils-${version}.tar
-cd elfutils-${version}/
-mkdir build
-cd build
-../configure --disable-libdebuginfod --disable-debuginfod --prefix=/elfutils
-make install -j${build_jobs}
-cd /
-rm -rf elfutils-${version}/ elfutils-${version}.tar
-
-
-version=$(grep boost dyninst/docker/dependencies.versions | awk '{split($0,a,":"); print a[2]}')
-wget --no-check-certificate https://boostorg.jfrog.io/artifactory/main/release/${version}/source/boost_${version//./_}.tar.bz2
-tar -xf boost_${version//./_}.tar.bz2
-cd boost_${version//./_}
-./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,thread,timer --prefix=/boost
-./b2 --prefix=/boost --layout=versioned variant=release link=shared threading=multi runtime-link=shared install
-cd /
-rm -rf boost_${version//./_} boost_${version//./_}.tar.bz2
-
-
-version=$(grep tbb dyninst/docker/dependencies.versions | awk '{split($0,a,":"); print a[2]}' | sed 's/\./_U/')
-wget --no-check-certificate https://github.com/oneapi-src/oneTBB/archive/refs/tags/${version}.tar.gz
-tar -xf ${version}.tar.gz
-dir_name=$(echo ${version} | sed 's/v//g')
-cd oneTBB-${dir_name}/
-make tbb_build_dir=install work_dir=working -j${build_jobs}
-mkdir -p /tbb/lib
-cp -R include /tbb/
-cp working_release/*.so* working_debug/*.so* /tbb/lib
-cd cmake
-cat << END >CMakeLists.txt
-cmake_minimum_required(VERSION 3.13.0)
-project(TBB-build)
-include(TBBMakeConfig.cmake)
-tbb_make_config(
-  TBB_ROOT /tbb
-  CONFIG_FOR_SOURCE
-  TBB_RELEASE_DIR /tbb/lib
-  TBB_DEBUG_DIR /tbb/lib
-)
-END
-cmake .
-cd /
-rm -rf ${version}.tar.gz oneTBB-${dir_name}
+git clone --depth=1 git://sourceware.org/git/elfutils.git elfutils-src
+git clone --depth=1 --recursive https://github.com/boostorg/boost.git boost-src
+git clone --depth=1 --branch=cmake_modernization https://github.com/dyninst/dyninst dyninst-src
+git clone --depth=1 https://github.com/oneapi-src/oneTBB.git tbb-src
---
+++ builds/min_deps_from_source/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+build_jobs=$1
+if test x"${build_jobs}" = "x"; then
+  build_jobs=1
+fi
+
+# Update sources
+for d in elfutils boost tbb dyninst; do
+  git -C /$d-src pull
+done
+
+cd /elfutils-src
+mkdir build; cd $_
+../configure --disable-libdebuginfod --disable-debuginfod --prefix=/elfutils
+make install -j${build_jobs}
+
+cd /boost-src
+./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,thread,timer --prefix=/boost
+./b2 --prefix=/boost --layout=versioned variant=release link=shared threading=multi runtime-link=shared install -j${build_jobs}
+
+cd /tbb-src
+mkdir build; cd $_
+cmake . -DCMAKE_INSTALL_PREFIX=/tbb -DTBB_TEST=OFF -DCMAKE_BUILD_TYPE=Release
+cmake --build . --parallel ${build_jobs}
+cmake --install .
+
+cd /dyninst-src
+mkdir build; cd $_
+cmake .. -DBoost_ROOT_DIR=/boost -DTBB_ROOT_DIR=/tbb -DElfUtils_ROOT_DIR=/elfutils -DDYNINST_WARNINGS_AS_ERRORS=ON
+cmake --build . --parallel $build_jobs
