60b16ab2c535cbcea201d9bfb6f8c2ff89f4056c
Tim Haines <thaines.astro@gmail.com>
2023-04-19 23:58:45 -0500

    instructionAPI

---
+++ instructionAPI/includes.cpp
@@ -0,0 +1,23 @@
+#include "ArchSpecificFormatters.h"
+#include "BinaryFunction.h"
+#include "Dereference.h"
+#include "Expression.h"
+#include "Immediate.h"
+#include "InstructionAST.h"
+#include "InstructionCategories.h"
+#include "InstructionDecoder.h"
+#include "Instruction.h"
+#include "Operand.h"
+#include "Operation_impl.h"
+#include "Register.h"
+#include "RegisterIDs.h"
+#include "Result.h"
+#include "Ternary.h"
+#include "Visitor.h"
+
+
+int main() {
+	namespace di = Dyninst::InstructionAPI;
+	di::ArchSpecificFormatter *f;
+	f->getInstructionString({});
+}
