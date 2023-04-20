adf5ce9567e8224423c4b2fa97ffa498367d8e16
Tim Haines <thaines.astro@gmail.com>
2023-04-20 17:40:22 -0500

    Add CMake option permutation test (#11)
    Ensure Dyninst can build using different combinations of its available user options

---
+++ builds/options/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+build_jobs=$1
+if test x"${build_jobs}" = "x"; then
+  build_jobs=1
+fi
+
+git clone --depth 1 --branch cmake_modernization https://github.com/dyninst/testsuite testsuite/src
+export BUILD_TEST_NUM_JOBS=${build_jobs}
+perl build.pl
