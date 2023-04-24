4d6ab6ede872eb5525f8179d863ddb7aa7702371
Tim Haines <thaines.astro@gmail.com>
2023-04-24 15:28:29 -0500

    deps_from_latest_source

--- builds/deps_from_latest_source/test.sh
+++ builds/deps_from_latest_source/test.sh
@@ -2,10 +2,23 @@
 
 set -e
 
-build_jobs=$1
-if test x"${build_jobs}" = "x"; then
-  build_jobs=1
-fi
+num_jobs=1
+log_file="/dev/null"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -n|--num-jobs)
+      num_jobs="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -l|--log-file)
+      log_file="$2"
+      shift # past argument
+      shift # past value
+      ;;
+  esac
+done
 
 # Update sources
 for d in elfutils boost tbb dyninst; do
@@ -14,18 +27,18 @@ done
 
 cd /elfutils-src
 autoreconf -i -f
-./configure --disable-libdebuginfod --disable-debuginfod --prefix=/elfutils --enable-maintainer-mode
-make install -j${build_jobs}
+./configure --disable-libdebuginfod --disable-debuginfod --prefix=/elfutils --enable-maintainer-mode >>$log_file 2>&1
+make install -j${num_jobs}
 
 cd /boost-src
-./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,thread,timer --prefix=/boost
-./b2 --prefix=/boost --layout=versioned variant=release link=shared threading=multi runtime-link=shared install -j${build_jobs}
+./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,thread,timer --prefix=/boost >>$log_file 2>&1
+./b2 --prefix=/boost --layout=versioned variant=release link=shared threading=multi runtime-link=shared install -j${num_jobs} >>$log_file 2>&1
 
 cd /tbb-src
-cmake . -DCMAKE_INSTALL_PREFIX=/tbb -DTBB_TEST=OFF -DCMAKE_BUILD_TYPE=Release
-cmake --build . --parallel ${build_jobs}
-cmake --install .
+cmake . -DCMAKE_INSTALL_PREFIX=/tbb -DTBB_TEST=OFF -DCMAKE_BUILD_TYPE=Release >>$log_file 2>&1
+cmake --build . --parallel ${num_jobs} >>$log_file 2>&1
+cmake --install . >>$log_file 2>&1
 
 cd /dyninst-src
-cmake . -DBoost_ROOT_DIR=/boost -DTBB_ROOT_DIR=/tbb -DElfUtils_ROOT_DIR=/elfutils -DDYNINST_WARNINGS_AS_ERRORS=ON
-cmake --build . --parallel $build_jobs
+cmake . -DBoost_ROOT_DIR=/boost -DTBB_ROOT_DIR=/tbb -DElfUtils_ROOT_DIR=/elfutils -DDYNINST_WARNINGS_AS_ERRORS=ON >>$log_file 2>&1
+cmake --build . --parallel $num_jobs >>$log_file 2>&1
