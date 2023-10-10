2bab11434e7dcd7c724bac401fad60f9f673bb99
Tim Haines <thaines.astro@gmail.com>
2023-10-10 16:04:25 -0500

    Add test for multiple CUs per Symtab module

---
+++ symtab/module/lib1.cpp
@@ -0,0 +1,7 @@
+#ifdef FUNC1
+extern "C" void lib1func1() {}
+#endif
+
+#ifdef FUNC2
+extern "C" void lib1func2() {}
+#endif
---
+++ symtab/module/lib2.cpp
@@ -0,0 +1,2 @@
+extern "C" void lib2func1() {}
+extern "C" void lib2func2() {}
