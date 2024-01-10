36a69fbbb460b75bd43d1881fd9c09b54c912cf7
Tim Haines <thaines.astro@gmail.com>
2024-01-10 10:49:49 -0600

    Add parseAPI/simpleParser (#24)
    This is both a good sanity check for PRs and can be used as a larger
    system-level parser for things like running through /usr/lib.

---
+++ parseAPI/simpleParser.cpp
@@ -0,0 +1,23 @@
+#include "CFG.h"
+#include "CodeObject.h"
+#include <iostream>
+
+namespace dp = Dyninst::ParseAPI;
+int main(int argc, char* argv[]) {
+  if(argc < 2 || argc > 3) {
+    std::cerr << "Usage: " << argv[0] << " file\n";
+    return -1;
+  }
+
+  try {
+    auto* sts = new dp::SymtabCodeSource(argv[1]);
+    auto* co = new dp::CodeObject(sts);
+    co->parse();
+  } catch(std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return -1;
+  } catch(...) {
+    std::cerr << "unknown exception occurred\n";
+    return -1;
+  }
+}
