682a5bd9ff8f3243065102bb7ce954189899291d
Tim Haines <thaines.astro@gmail.com>
2024-10-04 15:11:01 -0500

    Remove dyn{Elf,Dwarf} tests (#30)
    These are no longer part of the public API.

--- dwarf/includes.cpp
+++
@@ -1,8 +0,0 @@
-#include "dwarfExprParser.h"
-#include "dwarfFrameParser.h"
-#include "dwarfHandle.h"
-#include "dwarfResult.h"
-
-int main() {
-	auto d = Dyninst::DwarfDyninst::ConcreteDwarfResult{};
-}
